25af65c02419c46eb069ae028b57dbf8bfd2b91f
Sergio Esteve <sergio.esteve@atos.net>
2023-06-02 18:58:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# README
+
+```ps
+$path = pwd
+Get-ChildItem -Path $path.path -Include *.jpg,*.png -Recurse | Copy-Item -Destination ./img/
+```
+
+```ps
+pipenv shell
+mkdir <cert name>
+py .\main.py -file ".\path\to\exam.html"
+```
---
+++ main.py
@@ -0,0 +1,77 @@
+import argparse
+import json
+from bs4 import BeautifulSoup
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-file", "--examfile", help="HTML Exam file")
+args = parser.parse_args()
+file = args.examfile
+
+with open(file, "r", encoding="utf-8") as HTMLFile:
+    index = HTMLFile.readlines()
+    title = ""
+
+    file_title = ""
+
+    currentTitleI = 0
+    questionI = -1
+    currentBodyI = 0
+    currentOptionI = 0
+
+    questions = {}
+
+    for i, line in enumerate(index):
+        if index[i - 1].find("<h1") >= 0:
+            file_title = line.split("Exam Actual Questions")[0].strip()
+
+        if line.find("<div class=\"card-header text-white bg-primary\">") >= 0:
+            currentTitleI = i
+            questionI += 1
+        if i == currentTitleI + 1 and questionI > 0:
+            title = line.strip().replace("#", "")
+        if i == currentTitleI + 3 and questionI > 0:
+            title = line.strip() + " | " + title
+            title = " ".join([word.rjust(2, "0") for word in title.split(" ") if word != "|"])
+            questions[title] = {
+                "options": [],
+                "body": [],
+                "answer": ""
+            }
+
+        if line.find("<p class=\"card-text\">") >= 0:
+            currentBodyI = i
+        if i == currentBodyI + 3 and title in questions:
+
+            paragraphs = line.strip().split("<br>")
+            for p in paragraphs:
+                if p.find("img") > 0:
+                    src = p.split('"')[1].split("/")[-1]
+                    p = f"<img>/{file_title}/img/{src}<img>"
+                questions[title]["body"].append(p.strip())
+            # questions[title]["body"] =
+
+        if line.find("<li class=\"multi-choice-item") >= 0:
+            currentOptionI = i
+        if i == currentOptionI + 6 and title in questions:
+            questions[title]["options"].append(line.strip())
+
+        if line.find("<script id=") >= 0 and title in questions:
+            questions[title]["answer"] = line[line.find('", "vote_count"') - 1]
+
+        if line.find("<span class=\"correct-answer\"><img") >= 0:
+            src = line.split('/')[2].split('"')[0]
+            questions[title]["options"].append(f"<img>/{file_title}/img/{src}<img>")
+            
+
+    for question_title in questions:
+        question_formatted = {
+            "title": question_title,
+            "body": questions[question_title]["body"],
+            "options": questions[question_title]["options"],
+            "answer": questions[question_title]["answer"]
+        }
+        json_file = f"./{file_title.replace(' ', '_')}/{question_title.replace(' | ', '_').replace(' ', '_')}.json"
+        print(json_file)
+        with open(json_file, "w") as end_file:
+            json.dump(question_formatted, end_file)
